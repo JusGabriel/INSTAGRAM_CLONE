637c851712af2fc7cbc721cf63b91bf71b54a0ef
JusGabriel <168014809+JusGabriel@users.noreply.github.com>
2024-05-15 12:02:29 -0500

    Add files via upload

---
+++ ejercicio3_4.py
@@ -0,0 +1,23 @@
+print("Instagram")
+
+user =input("phone number, username or email: ")
+password = input ("password: ")
+
+if user == "justin" and password == "123":
+    print (f"Bienvenido {user}")
+    print (f"Loading your posts")
+else:
+    print ("Tu contraseña no es correcta. Compruébala.")
+
+print ("Sing up to see photos and videos from your frinds")
+input("log in with facebook? ")
+facebook=input ("Yes or No: ")
+if facebook == "yes":
+    print("Welcome")
+else:
+    mobile = input ("mobile number or email")
+    name= input ("Full name")
+    username = input ("Username")
+    password= input("password")
+
+
